9b85f6aecfe34dc0065504b9b8945348bd664d2a
John Lennie <25914398+john-lennie@users.noreply.github.com>
2025-02-18 23:03:30 -0500

    css

--- app/page.tsx
+++ app/page.tsx
@@ -72,7 +72,7 @@ export default function Home() {
                       ))}
                     </div>
                     <div className="aspect-w-16 aspect-h-9">
-                      <video src={project.video} playsinline autoplay muted loop preload="auto" className="w-full h-full object-cover">
+                      <video src={project.video} muted playsinline autoplay loop className="w-full h-full object-cover">
                         Your browser does not support the video tag.
                       </video>
                     </div>
