b9cd6ffb330741e6a5f076217c546bcc2093fbbd
John Lennie <25914398+john-lennie@users.noreply.github.com>
2025-02-19 00:17:52 -0500

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -72,7 +72,7 @@ export default function Home() {
                       ))}
                     </div>
                     <div className="aspect-w-16 aspect-h-9">
-                      <video src={project.video} muted playsinline autoplay loop className="w-full h-full object-cover">
+                      <video src={project.video} muted playsInline autoPlay loop className="w-full h-full object-cover">
                         Your browser does not support the video tag.
                       </video>
                     </div>
