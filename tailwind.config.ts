2d92de851ba097ed4d314d935bda55648c94bf18
John Lennie <25914398+john-lennie@users.noreply.github.com>
2025-02-20 23:08:04 -0500

    commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -95,7 +95,7 @@ const config: Config = {
 				'fade-in-fast': 'fade-in .25s ease-in-out',
 				'fade-in': 'fade-in .5s ease-in-out',
 				'fade-in-slow': 'fade-in 1s ease-in-out',
-				'fade-in-slower': 'fade-in 2s 2s ease-in-out'
+				'fade-in-slow-delay': 'fade-in 1s 1s ease-in-out'
   		}
   	}
   },
