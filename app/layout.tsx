e4bc42cd77feeda93684a0e645ba687a222ae493
John Lennie <25914398+john-lennie@users.noreply.github.com>
2025-03-30 23:18:59 -0400

    commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'Temp Portfolio',
-  description: 'Created with v0',
+  title: 'JNPR',
+  description: 'Digital Product Studio',
   generator: 'v0.dev',
 }
 
