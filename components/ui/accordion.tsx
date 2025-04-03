a222c5ced6142780d28a0cb64f124a7ca7c4358b
John Lennie <25914398+john-lennie@users.noreply.github.com>
2025-04-03 13:18:44 -0400

    commit

--- components/ui/accordion.tsx
+++ components/ui/accordion.tsx
@@ -50,7 +50,7 @@ const AccordionContent = React.forwardRef<
     )}
     {...props}
   >
-    <div className="p-2">{children}</div>
+    <div className="pt-0">{children}</div>
   </AccordionPrimitive.Content>
 ))
 AccordionContent.displayName = AccordionPrimitive.Content.displayName
