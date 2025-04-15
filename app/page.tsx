66f9d07b4d67e5f02beba04c4a94c41286d1dcdc
John Lennie <25914398+john-lennie@users.noreply.github.com>
2025-04-15 19:33:30 -0400

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -94,7 +94,7 @@ export default function Home() {
                         </div>
                       }
                       {project.mobileVideo &&
-                        <div className="flex justify-center py-12">
+                        <div className="flex justify-center py-16">
                           <div className="aspect-w-16 aspect-h-9 w-1/2 lg:w-1/3">
                             <video src={project.mobileVideo} muted playsInline autoPlay loop className="w-full h-full object-cover">
                               Your browser does not support the video tag.
