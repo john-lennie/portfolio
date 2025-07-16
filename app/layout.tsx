0da766d2251b54f8c8750b65a7fd68e6d6d73d6c
John Lennie <25914398+john-lennie@users.noreply.github.com>
2025-07-16 16:50:58 -0400

    commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ const sourceCodePro = Source_Code_Pro({
 
 export const metadata: Metadata = {
   title: 'JNPR Studio',
-  description: 'JNPR specializes in building websites for design-led clients — combining front-end engineering, a deep understanding of contemporary design, and digital strategy.'
+  description: 'JNPR is a digital design and engineering studio based in Toronto.'
 }
 
 export default function RootLayout({
