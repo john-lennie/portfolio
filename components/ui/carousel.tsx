5d00cfcdc1a320874b7e7c427d9b19d4d9587a79
John Lennie <25914398+john-lennie@users.noreply.github.com>
2025-08-05 15:00:08 -0400

    commit

--- components/ui/carousel.tsx
+++ components/ui/carousel.tsx
@@ -158,7 +158,7 @@
     const { carouselRef, orientation } = useCarousel()
 
     return (
-      <div ref={carouselRef} className="overflow-hidden pb-4">
+      <div ref={carouselRef} className="overflow-hidden">
         <div
           ref={ref}
           className={cn(
