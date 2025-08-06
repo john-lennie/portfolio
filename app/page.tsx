4a6e6efef56311c53e82615789558b976264b726
John Lennie <25914398+john-lennie@users.noreply.github.com>
2025-08-06 18:26:46 -0400

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -40,7 +40,7 @@ const projects = [
       } 
     ],
     description: [
-      "Worked with the VF Digital Technology Team to develop marketing pages, build on design systems, and perform accessibility audits/improvements for VF's umbrella of brands including The North Face, Vans, and Timberland.",
+      "Worked with VF Digital Tech Team to build marketing pages, enhance design systems, and improve accessibility for The North Face, Vans & Timberland.",
     ],
     stackTags: {
       frontEnd: "Nuxt",
