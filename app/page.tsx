b5da68882e5abf038d3fa346749b4c59c2393fe8
John Lennie <25914398+john-lennie@users.noreply.github.com>
2025-09-08 17:21:28 -0400

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -440,7 +440,6 @@ export default function Home() {
             <div className="col-span-2 max-w-[80%] lg:col-span-1 lg:row-span-2 mb-16 lg:mb-0 text-xs lg:w-full md:max-w-[70%]">
               <p className="mb-4 text-xs">Info:</p>
               <p>JNPR is a digital product design & engineering studio based in Toronto.</p>
-              <p>We help clients realize creative ideas & optimize user engagement through data-driven insights.</p>
               <p>
                 Interested in working together?<br />
                 Say hi, or <CopyButton textToCopy="hello@jnpr.studio" /><br />
