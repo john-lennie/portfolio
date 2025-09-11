59404e4256c27f5efb630d744d6c210976abc1fc
John Lennie <25914398+john-lennie@users.noreply.github.com>
2025-09-11 13:57:16 -0400

    removed duplicate css file

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from 'next'
 import './globals.css'
-import { GlobalStyles } from "@/components/GlobalStyles"
 import Header from "@/components/header"
 import Footer from "@/components/footer"
 import { Analytics } from "@vercel/analytics/next"
@@ -28,7 +27,6 @@ export default function RootLayout({
   return (
     <html lang="en" className={sourceCodePro.variable}>
       <body>
-        <GlobalStyles />
         <Header />
         {children}
         <Footer />
